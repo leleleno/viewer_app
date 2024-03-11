c8cc191477d54d649848964cf7f14fb731cadf39
leleleno <gakushimodaila@gmail.com>
2024-03-11 11:26:41 +0900

    github icon and link added

--- lib/pages/uis.dart
+++ lib/pages/uis.dart
@@ -1,10 +1,12 @@
 import 'package:first_app/data/searchdata.dart';
+import 'package:first_app/data/settingsdata.dart';
 import 'package:first_app/pages/favorite.dart';
 import 'package:first_app/pages/history.dart';
 import 'package:first_app/pages/search.dart';
 import 'package:first_app/pages/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class CommonScaffold extends StatefulWidget {
   final String title;
@@ -81,95 +83,113 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.inversePrimary),
-            child: const Center(child: Text('YGO wiki Viewer')),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('ホーム'),
-            selected: index == 0,
-            onTap: () {
-              // Drawer閉じる
-              Navigator.of(context).pop;
-              // Homeに戻る
-              Navigator.of(context).popUntil((route) => route.isFirst);
-            },
-          ),
-          Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              return ListTile(
-                leading: const Icon(Icons.search),
-                title: const Text('検索'),
-                selected: index == 1,
+    return Consumer(
+      builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        final settings = ref.watch(settingsNotifierProvider);
+        bool isDark = settings['isDark'];
+        return Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.inversePrimary),
+                child: const Center(child: Text('YGO wiki Viewer')),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('ホーム'),
+                selected: index == 0,
                 onTap: () {
-                  // ドロワー閉じる
+                  // Drawer閉じる
+                  Navigator.of(context).pop;
+                  // Homeに戻る
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                },
+              ),
+              Consumer(
+                builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                  return ListTile(
+                    leading: const Icon(Icons.search),
+                    title: const Text('検索'),
+                    selected: index == 1,
+                    onTap: () {
+                      // ドロワー閉じる
+                      Navigator.pop(context);
+                      // もし検索画面じゃなかったら
+                      if (index != 1) {
+                        // 検索ワードリセット
+                        // final notifier =
+                        //     ref.read(searchWordNotifierProvider.notifier);
+                        // notifier.addSearchWord("");
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const Search(),
+                          settings: const RouteSettings(name: "/search"),
+                        ));
+                      }
+                    },
+                  );
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.favorite),
+                title: const Text('お気に入り'),
+                selected: index == 2,
+                onTap: () {
                   Navigator.pop(context);
-                  // もし検索画面じゃなかったら
-                  if (index != 1) {
-                    // 検索ワードリセット
-                    // final notifier =
-                    //     ref.read(searchWordNotifierProvider.notifier);
-                    // notifier.addSearchWord("");
+                  if (index != 2) {
                     Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const Search(),
-                      settings: const RouteSettings(name: "/search"),
+                      builder: (context) => const Favorite(),
+                      settings: const RouteSettings(name: "/favorite"),
                     ));
                   }
                 },
-              );
-            },
+              ),
+              ListTile(
+                leading: const Icon(Icons.history),
+                title: const Text('履歴'),
+                selected: index == 3,
+                onTap: () {
+                  Navigator.pop(context);
+                  if (index != 3) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const History(),
+                      settings: const RouteSettings(name: "/history"),
+                    ));
+                  }
+                },
+              ),
+              const Divider(),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('設定'),
+                selected: index == 4,
+                onTap: () {
+                  Navigator.pop(context);
+                  if (index != 4) {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Settings(),
+                      settings: const RouteSettings(name: "/settings"),
+                    ));
+                  }
+                },
+              ),
+              ListTile(
+                leading: ImageIcon(
+                isDark
+                  ? const AssetImage('assets/icons/github-mark-white.png')
+                  : const AssetImage('assets/icons/github-mark.png'),),
+                title: const Text('Github link'),
+                selected: false,
+                onTap: () {
+                  Navigator.pop(context);
+                  launchUrlString("https://github.com/leleleno/viewer_app");
+                },
+              ),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.favorite),
-            title: const Text('お気に入り'),
-            selected: index == 2,
-            onTap: () {
-              Navigator.pop(context);
-              if (index != 2) {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const Favorite(),
-                  settings: const RouteSettings(name: "/favorite"),
-                ));
-              }
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.history),
-            title: const Text('履歴'),
-            selected: index == 3,
-            onTap: () {
-              Navigator.pop(context);
-              if (index != 3) {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const History(),
-                  settings: const RouteSettings(name: "/history"),
-                ));
-              }
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('設定'),
-            selected: index == 4,
-            onTap: () {
-              Navigator.pop(context);
-              if (index != 4) {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const Settings(),
-                  settings: const RouteSettings(name: "/settings"),
-                ));
-              }
-            },
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
